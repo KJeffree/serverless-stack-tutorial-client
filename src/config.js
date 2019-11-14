8ac1be58c165760cb45c4f139880a3bd0e1f8e1d
KJeffree <k.jeffree@gmail.com>
2019-11-14 10:44:44 +0000

    setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-2",
+      BUCKET: "notes-application-uploads-aws-tutorial"
+    },
+    apiGateway: {
+      REGION: "eu-west-2",
+      URL: "https://f8ot4q0817.execute-api.eu-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-2",
+      USER_POOL_ID: "eu-west-2_QoUVk1mXo",
+      APP_CLIENT_ID: "b1gp9rtgffuo3iijl1peca3gp",
+      IDENTITY_POOL_ID: "eu-west-2:176e1bb6-b3c0-4e53-89ad-7f5eb5372641"
+    }
+  };
